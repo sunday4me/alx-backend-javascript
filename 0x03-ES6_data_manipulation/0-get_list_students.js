2d37774ae06fd2a82cc3fb55ad0ad4ecd61363d3
Sunday  Jimoh <31360475+sunday4me@users.noreply.github.com>
2023-03-27 16:13:01 +0100

    Solution to task zero

--- 0x03-ES6_data_manipulation/0-get_list_students.js
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -1,1 +1,12 @@
-
+/**
+ * Retrieves a list of students.
+ * @author Sunday Jimoh <https://github.com/sunday4me>
+ * @returns {{id: Number, firstName: String, location: String}[]}
+ */
+export default function getListStudents() {
+  return [
+    { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
+    { id: 2, firstName: 'James', location: 'Columbia' },
+    { id: 5, firstName: 'Serena', location: 'San Francisco' },
+  ];
+}
