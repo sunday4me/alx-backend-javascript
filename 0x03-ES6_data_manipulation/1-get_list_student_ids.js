4878f12fcd585dc880bcf7920ab9d883fd5dba42
Sunday  Jimoh <31360475+sunday4me@users.noreply.github.com>
2023-03-27 16:14:31 +0100

    Solution to task one

--- 0x03-ES6_data_manipulation/1-get_list_student_ids.js
+++ 0x03-ES6_data_manipulation/1-get_list_student_ids.js
@@ -1,1 +1,16 @@
-
+/**
+ * Retrieves ids from a list of students.
+ * @param {{
+ *   id: Number,
+ *   firstName: String,
+ *   location: String
+ * }[]} students - The list of students.
+ * @author Sunday Jimoh <https://github.com/sunday4me>
+ * @returns
+ */
+export default function getListStudentIds(students) {
+  if (students instanceof Array) {
+    return students.map((student) => student.id);
+  }
+  return [];
+}
