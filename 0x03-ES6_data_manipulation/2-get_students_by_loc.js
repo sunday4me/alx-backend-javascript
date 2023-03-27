356fe4fa20a879980a2228d760df56f52b90873b
Sunday  Jimoh <31360475+sunday4me@users.noreply.github.com>
2023-03-27 16:21:54 +0100

    Solution to task two

--- 0x03-ES6_data_manipulation/2-get_students_by_loc.js
+++ 0x03-ES6_data_manipulation/2-get_students_by_loc.js
@@ -1,1 +1,17 @@
-
+/**
+ * Retrieves students in a given location.
+ * @param {{
+ *   id: Number,
+ *   firstName: String,
+ *   location: String
+ * }[]} students - The list of students.
+ * @param {String} city - The location.
+ * @author Sunday Jimoh <https://github.com/sunday4me>
+ * @returns
+ */
+export default function getStudentsByLocation(students, city) {
+  if (students instanceof Array) {
+    return students.filter((student) => student.location === city);
+  }
+  return [];
+}
