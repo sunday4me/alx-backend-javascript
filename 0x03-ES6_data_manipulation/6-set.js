990a43361c69e3c8fe803f64fb5ce273f4591510
Sunday  Jimoh <31360475+sunday4me@users.noreply.github.com>
2023-03-27 16:29:16 +0100

    Solution to task six

--- 0x03-ES6_data_manipulation/6-set.js
+++ 0x03-ES6_data_manipulation/6-set.js
@@ -1,1 +1,9 @@
-
+/**
+ * Creates a set from an array.
+ * @param {Array} array - The source array.
+ * @author Sunday Jimoh <https://github.com/sunday4me>
+ * @returns {Set}
+ */
+export default function setFromArray(array) {
+  return new Set(array);
+}
